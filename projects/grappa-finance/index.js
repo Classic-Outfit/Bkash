c161a57a544d9f0d4d0c76605e91ee1430ebf83f
Classic Antor <iamantor78@gmail.com>
2026-09-25 22:56:53 +0600

    Initialize revert-17894-main

---
+++ projects/grappa-finance/index.js
@@ -0,0 +1,12 @@
+const ethereumTvl = require('./grappa-ethereum');
+
+module.exports = {
+  ethereum: {
+    start: '2023-02-04',
+    tvl: ethereumTvl,
+  },
+  hallmarks: [
+    ['2023-02-24', "Hashnote vault launch"],
+  ]
+}
+ 
